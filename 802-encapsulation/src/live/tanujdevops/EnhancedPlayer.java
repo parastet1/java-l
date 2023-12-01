8f5186950499a7278dde23b57ead1222ecc2c2f4
Tanuj Paraste <tanuj.paraste1@gmail.com>
2023-12-01 16:58:40 +0530

    updated files: internal changes in an encapsulated object does not affect other code as long as public interfaces are intact

--- 802-encapsulation/src/live/tanujdevops/EnhancedPlayer.java
+++ 802-encapsulation/src/live/tanujdevops/EnhancedPlayer.java
@@ -1,8 +1,8 @@
 package live.tanujdevops;
 
 public class EnhancedPlayer {
-	private String name;
-	private int health;
+	private String fullName;
+	private int healthPercentage;
 	private String weapon;
 
 	public EnhancedPlayer(String name) {
@@ -10,35 +10,35 @@ public class EnhancedPlayer {
 		this(name, 100, "Sword");
 	}
 
-	public EnhancedPlayer(String name, int health, String weapon) {
+	public EnhancedPlayer(String fullName, int health, String weapon) {
 		super();
-		this.name = name;
+		this.fullName = fullName;
 		if (health <= 0) {
-			this.health = 1;
+			this.healthPercentage = 1;
 		} else if (health > 100) {
-			this.health = 100;
+			this.healthPercentage = 100;
 		} else {
-			this.health = health;
+			this.healthPercentage = health;
 		}
 		this.weapon = weapon;
 	}
 
 	public void loseHealth(int damage) {
-		health = health - damage;
-		if (health <= 0) {
+		healthPercentage = healthPercentage - damage;
+		if (healthPercentage <= 0) {
 			System.out.println("Player knocked out of game");
 		}
 	}
 
 	public int healthRemaining() {
-		return health;
+		return healthPercentage;
 	}
 
 	public void restoreHealth(int extraHealth) {
-		health = health + extraHealth;
-		if (health > 100) {
+		healthPercentage = healthPercentage + extraHealth;
+		if (healthPercentage > 100) {
 			System.out.println("Player restored to 100%");
-			health = 100;
+			healthPercentage = 100;
 		}
 	}
 }
