88e24f18dd7f6688cc06167bb7e21cad0ad0769b
Tanuj Paraste <tanuj.paraste1@gmail.com>
2023-12-30 20:20:27 +0530

    updated files: private fields are not accessible outside the class

--- 701-Classes/src/Main.java
+++ 701-Classes/src/Main.java
@@ -2,6 +2,9 @@
 public class Main {
 	public static void main(String[] args) {
 		Car myCar = new Car();
+		myCar.make = "Porsche";
+		myCar.model = "Carrera";
+		myCar.color = "Red";
 		myCar.describeCar();
 	}
 }
