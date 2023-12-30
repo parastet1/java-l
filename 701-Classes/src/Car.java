cf75dde98a6bf600f58d03c8e3bb37d044ae06db
Tanuj Paraste <tanuj.paraste1@gmail.com>
2023-12-30 20:18:55 +0530

    updated files: assigning default value to fields of a class

--- 701-Classes/src/Car.java
+++ 701-Classes/src/Car.java
@@ -1,10 +1,10 @@
 
 public class Car {
-	private String make;
-	private String model;
-	private String color;
-	private int doors;
-	private boolean convertible;
+	private String make = "Tesla";
+	private String model = "Model X";
+	private String color = "Gray";
+	private int doors = 2;
+	private boolean convertible = true;
 
 	public void describeCar() {
 		System.out.printf("%d-Door %s %s %s %s\n", doors, color, make, model, convertible ? "Convertible" : "");
