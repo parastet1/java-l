3db9c29662c074744ed608de9570b4c7dfb820e4
Tanuj Paraste <tanuj.paraste1@gmail.com>
2024-01-02 17:59:39 +0530

    added files: working iwth pojo

---
+++ 704-POJO/src/Main.java
@@ -0,0 +1,8 @@
+
+public class Main {
+	public static void main(String[] args) {
+		for (int i = 1; i <= 5; i++) {
+			Student s = new Student("S2023" + i, "John", "01/01/2023", "Java Masterclass");
+		}
+	}
+}
---
+++ 704-POJO/src/Student.java
@@ -0,0 +1,14 @@
+
+public class Student {
+	private String id;
+	private String name;
+	private String dateOfBirth;
+	private String classList;
+
+	public Student(String id, String name, String dateOfBirth, String classList) {
+		this.id = id;
+		this.name = name;
+		this.dateOfBirth = dateOfBirth;
+		this.classList = classList;
+	}
+}
