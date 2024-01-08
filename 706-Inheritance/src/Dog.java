ce6c5ba4f6052c606b41706470bdaf448ad33f47
Tanuj Paraste <tanuj.paraste1@gmail.com>
2024-01-08 21:19:35 +0530

    updated files: using methods and constructors from superclass

--- 706-Inheritance/src/Dog.java
+++ 706-Inheritance/src/Dog.java
@@ -1,7 +1,28 @@
 
 public class Dog extends Animal {
+	private String earShape;
+	private String tailShape;
 
 	public Dog() {
 		super("Matt", "Big", 50);
 	}
+
+	public Dog(String type, double weight) {
+		this(type, weight, "Perky", "Curled");
+	}
+
+	public Dog(String type, double weight, String earShape, String tailShape) {
+		super(type, weight < 15 ? "small" : (weight < 53 ? "medium" : "large"), weight);
+		this.earShape = earShape;
+		this.tailShape = tailShape;
+	}
+
+	@Override
+	public String toString() {
+		StringBuilder builder = new StringBuilder();
+		builder.append("Dog [earShape=").append(earShape).append(", tailShape=").append(tailShape)
+				.append(", super=").append(super.toString()).append("]");
+		return builder.toString();
+	}
+
 }
